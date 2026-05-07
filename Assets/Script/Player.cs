7ca042208341ee905aca859b037b9d39142c51ba
Fernando Camoleze Urccoviche <fernando.camoleze@docente.fieb.edu.br>
2026-05-07 09:11:48 -0300

    Atualização

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,59 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // ← namespace novo obrigatório!
 
 public class Player : MonoBehaviour
 {
-
-    [Range(0,10)]public int velocidade;
+    [Range(0, 10)] public int velocidade;
     Rigidbody2D rig;
 
-    private Vector2 posicaoMouse; //armazena a posição do mouse
-    public Camera cameraDoJogo; //conectar o script ao objeto Main Camera
+    // Referência à câmera (igual antes)
+    public Camera cameraDoJogo;
 
-    //variaveis do tiro
-    public GameObject laser; //objeto que será criado
-    public Transform disparo; // local onde será criado
+    // Variáveis do tiro (igual antes)
+    public GameObject laser;
+    public Transform disparo;
     public AudioSource somLazer;
     public GameController controller;
-  
-    // Start is called before the first frame update
-    void Start()
+
+    // ===== NOVO INPUT SYSTEM =====
+    // Criamos variáveis do tipo InputAction para cada ação
+    private InputAction moverAction;    // vai substituir Input.GetAxis
+    private InputAction atirarAction;   // vai substituir Input.GetButtonDown
+
+    void Awake()
     {
-        rig=GetComponent<Rigidbody2D>();
-        somLazer=GetComponent<AudioSource>();
+        // Aqui conectamos nossas variáveis às ações que existem
+        // no InputSystem_Actions (aquele arquivo criado no passo 3)
+        // "Player/Move" = Action Map "Player", ação "Move"
+        moverAction = InputSystem.actions.FindAction("Player/Move");
+        atirarAction = InputSystem.actions.FindAction("Player/Attack");
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        somLazer = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         mover();
         disparar();
     }
 
-    void mover(){
-        rig.linearVelocity=new Vector2(Input.GetAxis("Horizontal")* velocidade, Input.GetAxis("Vertical") * velocidade); //rig.velocity.y
-        //rig.MovePosition(rig.position + (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")) * velocidade);
-        posicaoMouse=cameraDoJogo.ScreenToWorldPoint(Input.mousePosition); //armazena a localização do Mouse na tela
-        
-        Vector2 distancia=posicaoMouse - rig.position;
-        float anguloMira=Mathf.Atan2(distancia.y,distancia.x) * Mathf.Rad2Deg;
-        rig.rotation=anguloMira;
+    void mover()
+    {
+        // ANTES: Input.GetAxis("Horizontal") e Input.GetAxis("Vertical")
+        // AGORA: lemos um Vector2 direto da ação "Move"
+        Vector2 direcao = moverAction.ReadValue<Vector2>();
+        rig.linearVelocity = direcao * velocidade;
+
+        // ANTES: Input.mousePosition
+        // AGORA: Mouse.current.position (precisa do using UnityEngine.InputSystem)
+        Vector2 posicaoMouse = cameraDoJogo.ScreenToWorldPoint(
+            Mouse.current.position.ReadValue()
+        );
+
+        Vector2 distancia = posicaoMouse - rig.position;
+        float anguloMira = Mathf.Atan2(distancia.y, distancia.x) * Mathf.Rad2Deg;
+        rig.rotation = anguloMira;
     }
 
-    void disparar(){
-        if(Input.GetButtonDown("Fire1")){ //Fire1 Ctrl esquerdo e botão esquerdo mouse
-            Instantiate(laser,disparo.position,disparo.rotation);
+    void disparar()
+    {
+        // ANTES: Input.GetButtonDown("Fire1")
+        // AGORA: WasPressedThisFrame() — verifica se apertou neste frame
+        if (atirarAction.WasPressedThisFrame())
+        {
+            Instantiate(laser, disparo.position, disparo.rotation);
             somLazer.Play();
         }
     }
 
-    void OnTriggerEnter2D(Collider2D bateu){
-        if(bateu.gameObject.tag=="x"){
-            transform.position= new Vector3(transform.position.x * -0.9f,transform.position.y,transform.position.z);
+    void OnTriggerEnter2D(Collider2D bateu)
+    {
+        // Esse trecho NÃO muda! Colisão não tem nada a ver com input
+        if (bateu.gameObject.tag == "x")
+        {
+            transform.position = new Vector3(
+                transform.position.x * -0.9f,
+                transform.position.y,
+                transform.position.z
+            );
         }
-                if(bateu.gameObject.tag=="y"){
-            transform.position= new Vector3(transform.position.x,transform.position.y * -0.9f,transform.position.z);
+        if (bateu.gameObject.tag == "y")
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y * -0.9f,
+                transform.position.z
+            );
         }
     }
 }
